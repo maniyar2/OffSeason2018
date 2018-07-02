6faaec9c395c3f3135c1a50e5662d4fbecaee738
maniyar1 <kamalisarvestani.1@themetroschool.org>
2018-07-02 19:39:55 -0400

    Got drivetrain finished, need to be tested debugged

--- src/frc/team3324/Robot.kt
+++ src/frc/team3324/Robot.kt
@@ -1,11 +1,16 @@
 package frc.team3324.robot
 
+import frc.team3324.subsystems.DriveTrain
+
 import edu.first.wpilibj.*
 import edu.wpi.first.wpilibj.command.Scheduler
 
 class Robot: IterativeRobot() {
-    companion object {
 
+
+    companion object {
+    val scheduler: Scheduler = Scheduler.getInstance()
+    public val mDriveTrain = DriveTrain;
     }
 
     override fun robotInit() {
@@ -28,10 +33,10 @@ class Robot: IterativeRobot() {
 
     }
     override fun teleopInit() {
-
+	scheduler.add(new DriveGroup());
     }
 
     override fun teleopPeriodic() {
-
+    scheduler.run();
     }
 }
--- src/frc/team3324/commands/teleop/DriveTank.kt
+++ src/frc/team3324/commands/teleop/DriveTank.kt
@@ -2,4 +2,19 @@ package frc.team3324.commands.teleop;
 
 import frc.team3324.Robot;
 import frc.team3324.subsystems.DriveTrain;
+import frc.team3324.OI;
 
+class DriveTank() {
+    init {
+        this.requires(DriveTrain)
+        this.isInterruptible = true
+    }
+    override fun execute() {
+        leftY = OI.get0LeftY;
+        rightX = OI.get0RightX;
+        Robot.mDriveTrain.arcadeDrive(leftY, rightX)
+    }
+    override fun isFinished() = false
+    override fun end() = Robot.mDriveTrain.arcadeDrive(0, 0)
+    override fun interrupted() = this.end()
+}
